c92139fec37a0eb854b2a79747b0e8361e70c117
zatb <zatbolanos@gmail.com>
2024-05-22 22:19:15 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -5,4 +5,9 @@ const calcTip = bill => {
     const tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
     return tip;
 }
+//Display a message in the console that includes the bill amount, the tip amount, and the total cost (bill plus tip).
+const bill1 = 150;
+const tip1 = calcTip(bill1);
+const total1 = bill1 + tip1;
+console.log(`The bill was $${bill1}, the tip was $${tip1}, and the total value $${total1}.`);
 
